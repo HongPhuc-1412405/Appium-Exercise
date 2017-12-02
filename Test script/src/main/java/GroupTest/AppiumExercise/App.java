e832ddfe52be0d9892fd784a79c35b7c038a2518
HongPhuc-1412405 <nhphuc030796@gmail.com>
2017-12-02 10:41:05 +0700

    Add test script and fiel apk

---
+++ Test script/src/main/java/GroupTest/AppiumExercise/App.java
@@ -0,0 +1,13 @@
+package GroupTest.AppiumExercise;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
